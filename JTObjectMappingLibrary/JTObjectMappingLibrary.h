139d54233821a948c078c1f939e3a4f8d41b91bf
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2012-11-11 16:45:57 +0900

    Add static library target

---
+++ JTObjectMappingLibrary/JTObjectMappingLibrary.h
@@ -0,0 +1,13 @@
+//
+//  JTObjectMappingLibrary.h
+//  JTObjectMappingLibrary
+//
+//  Created by Kataoka Naoyuki on 12/11/11.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTObjectMappingLibrary : NSObject
+
+@end
---
+++ JTObjectMappingLibrary/JTObjectMappingLibrary.m
@@ -0,0 +1,13 @@
+//
+//  JTObjectMappingLibrary.m
+//  JTObjectMappingLibrary
+//
+//  Created by Kataoka Naoyuki on 12/11/11.
+//
+//
+
+#import "JTObjectMappingLibrary.h"
+
+@implementation JTObjectMappingLibrary
+
+@end
